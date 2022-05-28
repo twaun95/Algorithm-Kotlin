375c64e388f96f44c14179c01892d1e1487e1f52
taewaun <taewaunkwon@gmail.com>
2022-05-28 17:47:09 +0900

    신고결과받기

---
+++ 프로그래머스/신고결과받기/solution.kt
@@ -0,0 +1,69 @@
+class Solution {
+    fun solution(id_list: Array<String>, report: Array<String>, k: Int): IntArray {
+        lateinit var answer: IntArray
+
+        var answerList: MutableList<Int> = mutableListOf()
+        var map: MutableMap<String, MutableSet<String>> = mutableMapOf() 
+        var kMap: MutableMap<String, Int> = mutableMapOf()
+
+        println(id_list.toList())
+
+        id_list.forEach{
+            map.put(it, mutableSetOf())
+            kMap.put(it, 0)
+        }
+
+        println(map)
+        println(kMap)
+
+        report.forEach{ 
+
+            it.split(" ").let { splitedReport ->
+                map[splitedReport.first()]!!.add(splitedReport.last())
+            }
+            
+        }
+
+        map.forEach {
+            it.value.forEach {
+                kMap[it] = kMap[it]!!.plus(1)
+            }
+        }
+
+        println(map)
+        println(kMap)
+
+
+        id_list.forEachIndexed { index, item ->
+            println(index)
+            println(item)
+            answerList.add(0)
+
+            map[item]!!.forEach {
+                if(kMap[it]!!>=k) {
+                    answerList[index] += 1 
+                }
+            }
+        }
+
+        println(answerList)
+
+
+
+        return answerList.toIntArray()
+    }
+}
+
+fun main() {
+    Solution().solution(
+        arrayOf("muzi", "frodo", "apeach", "neo"),
+        arrayOf("muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"),
+        2
+    )
+
+    // Solution().solution(
+    //     arrayOf("con", "ryan"),
+    //     arrayOf("ryan con", "ryan con", "ryan con", "ryan con"),
+    //     3
+    // )
+}
---
+++ 프로그래머스/신고결과받기/tempCodeRunnerFile.kt
@@ -0,0 +1,5 @@
+    // Solution().solution(
+    //     arrayOf("muzi", "frodo", "apeach", "neo"),
+    //     arrayOf("muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"),
+    //     2
+    // )
