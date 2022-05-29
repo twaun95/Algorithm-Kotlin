bc0b547c11835793aa629ce276062f8059e0fc86
taewaun <taewaunkwon@gmail.com>
2022-05-29 15:56:27 +0900

    신고 결과 받기

---
+++ 프로그래머스/신고결과받기/README.md
@@ -0,0 +1,9 @@
+# 신고 결과 받기
+# LEVEL 1
+# 2022 KAKAO BLIND RECRUITMENT
+
+
+
+- map, set, list 
+- split
+- .toIntArray() 
--- 프로그래머스/신고결과받기/tempCodeRunnerFile.kt
+++
@@ -1,5 +0,0 @@
-    // Solution().solution(
-    //     arrayOf("muzi", "frodo", "apeach", "neo"),
-    //     arrayOf("muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"),
-    //     2
-    // )
