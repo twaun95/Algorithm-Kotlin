b7e0186c7ce5b5761f7ddbe1662c3a10e1a073c0
taewaun <taewaunkwon@gmail.com>
2022-08-20 21:32:48 +0900

    큐 팁

--- 팁/큐.kt
+++ 팁/큐.kt
@@ -1,3 +1,18 @@
 fun main() = with(System.`in`.bufferedReader()) {
+    // 리스트를 마치 큐처럼
+    // FIFO
+    val queue = mutableListOf<Int>(1,2,3)
+
+
+    // in
+    queue.add(4)
+    println(queue)
+
+    // out
+    queue.removeAt(0)
+    println(queue)
+
+
+    // java.util. 을 import하여 LinkedList 를 이용하는 방법.
     
 }
