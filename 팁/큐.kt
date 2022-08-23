8de6e4b4b8c06fcb45c41924fe42a962ffa7f42e
taewaun <taewaunkwon@gmail.com>
2022-08-23 16:06:12 +0900

    큐

--- 팁/큐.kt
+++ 팁/큐.kt
@@ -1,25 +1,22 @@
+
+import java.util.LinkedList
+import java.util.Queue
+
 fun main() = with(System.`in`.bufferedReader()) {
-    // FIFO
+    // java.util. 을 import하여 LinkedList 를 이용하는 방법.
+    val queue: Queue<Int> = LinkedList()
 
-    // 1. 리스트를 마치 큐처럼
-    // 모두 사이즈 관련 에러는 Exeption
-    val queue = mutableListOf<Int>(1,2,3)
-
-
-    queue.add(4)
-    println(queue)
-
-    queue.removeAt(0)
-    println(queue)
-
-    println(queue.first())
-    println(queue.isEmpty())
-
-
-    // 2. java.util. 을 import하여 LinkedList 를 이용하는 방법.
 
     // add(Exception) / offer
-    // Remove(Exception) / Poll
-    // element(Exception), peak(null)
+    // Remove(Exception) / poll
+    // element(Exception), peek(null)
 
+    queue.offer(1)
+    queue.offer(2)
+    queue.offer(3)
+
+    println(queue)
+    queue.poll()
+    println(queue)
+    println(queue.peek())
 }
